bfe0af09486f228c8ab55562849ac6e204340893
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-03-02 23:42:34 -0300

    Commit
    Commit

---
+++ src/routes/api.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+
+const router = Router();
+
+// Rota de teste
+router.get('/ping', (req,res) => {
+    res.json({ pong: true })
+})
+
+router.get('/random', (req, res) => {
+    let nRand: number = Math.floor( Math.random() * 10)
+    res.json({ numero: nRand })
+})
+
+router.get('/nome/:nome', (req, res) => {
+    let nome: string = req.params.nome;
+    res.json({ nome })
+})
+
+export default router;
